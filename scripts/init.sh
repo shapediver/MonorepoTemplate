58c67f6bef56355372529a83e945cde035bfa365
Michael Oppitz <michael@shapediver.com>
2020-12-11 14:23:15 +0100

    small script changes for setup and publishing

--- scripts/init.sh
+++ scripts/init.sh
@@ -13,4 +13,24 @@ then
     json -q -I -f 'scope.json' -e 'this.scope="'$NEW_SCOPE'"'
 fi
 
+function ask_yes_or_no() {
+    read -p "$1 ([y]es or [n]o): "
+    case $(echo $REPLY | tr '[A-Z]' '[a-z]') in
+        y|yes) echo "yes" ;;
+        *)     echo "no" ;;
+    esac
+}
+
+INITIALIZED=$(json -f 'scope.json' initialized)
+
+if [ $INITIALIZED != 'true' ]
+then
+    echo 'The "npm run publish" command is currently set to publish all packages, every time (even if there were no changes).'
+    if [[ "yes" == $(ask_yes_or_no "Do you want to independently publish only changed packages instead?") ]]
+    then
+        json -q -I -f 'lerna.json' -e 'this.version="independent"'
+    fi
+fi
+
+json -q -I -f 'scope.json' -e 'this.initialized=true'
 npm run bootstrap
